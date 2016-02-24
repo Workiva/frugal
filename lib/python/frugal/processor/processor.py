a71c9974f75716dcc5b037f88b35baa1fd265fe4
Charlie Strawn <charlie.strawn@workiva.com>
2016-02-24 15:27:39 -0600

    Python: Handle Unknown Functions in FBaseProcessor

--- lib/python/frugal/processor/processor.py
+++ lib/python/frugal/processor/processor.py
@@ -1,3 +1,8 @@
+from threading import Lock
+
+from thrift.Thrift import TApplicationException
+from thrift.Thrift import TMessageType
+from thrift.Thrift import TType
 
 class FProcessor(object):
     """
@@ -13,13 +18,40 @@ class FBaseProcessor(FProcessor):
 
     def __init__(self, processor_function_map):
         self._processor_function_map = processor_function_map
+        self._write_lock = Lock()
 
     def process(self, iprot, oprot):
+        """ Process an input protocol and output protocol
+
+        Args:
+            iprot: input FProtocol
+            oport: ouput FProtocol
+
+        Raises:
+            TApplicationException: if the processor does not know how to handle
+                this type of function.
+        """
+
         context = iprot.read_request_header()
-        message = iprot.read_message_begin()
+        message = iprot.readMessageBegin()
         processor = self._processor_function_map.get(message.name)
+
         if processor:
             processor.process(context, iprot, oprot)
             return
 
-        # TODO
+        iprot.skip(TType.STRUCT)
+        iprot.readMessageEnd()
+
+        ex = TApplicationException(TApplicationException.UNKNOWN_METHOD,
+                                   "Unknown function: {0}".format(message.name))
+
+        with self._write_lock:
+            oprot.write_response_headers(context)
+            oprot.writeMessageBegin(message.name, TMessageType.EXCEPTION, 0)
+
+            ex.write(oprot)
+            oprot.writeMessageEnd()
+            oprot.trans.flush()
+
+        raise ex
