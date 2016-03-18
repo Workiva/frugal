057e3a44fa24b92b9886a4b1954c2a0f4267b60b
Tyler Treat <tyler.treat@webfilings.com>
2016-03-18 13:25:40 -0500

    Go: various transport fixes

--- lib/go/transport.go
+++ lib/go/transport.go
@@ -91,7 +91,7 @@ type fMuxTransport struct {
 	numWorkers          uint
 	workC               chan []byte
 	open                bool
-	mu                  sync.Mutex
+	mu                  sync.RWMutex
 	closed              chan error
 	monitorClosedSignal chan<- error
 }
@@ -115,6 +115,7 @@ func (f *fMuxTransport) SetMonitor(monitor FTransportMonitor) {
 	select {
 	case f.monitorClosedSignal <- nil:
 	default:
+		log.Println("frugal: dropped transport monitor close signal")
 	}
 
 	// Start the new monitor
@@ -202,6 +203,12 @@ func (f *fMuxTransport) Open() error {
 	return nil
 }
 
+func (f *fMuxTransport) IsOpen() bool {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	return f.open && f.TFramedTransport.IsOpen()
+}
+
 // Close will close the underlying TTransport and stops all goroutines.
 func (f *fMuxTransport) Close() error {
 	return f.close(nil)
@@ -233,6 +240,26 @@ func (f *fMuxTransport) close(cause error) error {
 	}
 
 	return nil
+}
+
+func (f *fMuxTransport) Read(p []byte) (int, error) {
+	f.mu.RLock()
+	open := f.open
+	f.mu.RUnlock()
+	if !open {
+		return 0, thrift.NewTTransportException(thrift.NOT_OPEN, "fMuxTransport not open")
+	}
+	return f.TFramedTransport.Read(p)
+}
+
+func (f *fMuxTransport) Write(p []byte) (int, error) {
+	f.mu.RLock()
+	open := f.open
+	f.mu.RUnlock()
+	if !open {
+		return 0, thrift.NewTTransportException(thrift.NOT_OPEN, "fMuxTransport not open")
+	}
+	return f.TFramedTransport.Write(p)
 }
 
 // Closed channel is closed when the FTransport is closed.
@@ -274,7 +301,7 @@ func (f *fMuxTransport) startWorkers() {
 }
 
 func (f *fMuxTransport) closedChan() <-chan error {
-	f.mu.Lock()
-	defer f.mu.Unlock()
+	f.mu.RLock()
+	defer f.mu.RUnlock()
 	return f.closed
 }
