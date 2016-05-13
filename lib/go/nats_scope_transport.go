16a433f645e637b257d0e6c26bc4eb421b959ee6
Charlie Strawn <charlie.strawn@workiva.com>
2016-05-13 14:03:26 -0500

    Python: Revert unintended committed file.

--- lib/go/nats_scope_transport.go
+++ lib/go/nats_scope_transport.go
@@ -147,7 +147,6 @@ func (n *fNatsScopeTransport) handleMessage(msg *nats.Msg) {
 		return
 	}
 	// Discard frame size.
-	fmt.Printf("Message data %s", msg.Data[4:])
 	select {
 	case n.frameBuffer <- msg.Data[4:]:
 	case <-n.closeChan:
