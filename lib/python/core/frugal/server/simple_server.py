e1482e0ce64742098ee2b8828c012489f334f3f6
Charlie Strawn <charlie.strawn@workiva.com>
2016-06-15 15:36:00 -0500

    Python: Remove simple server example.

--- lib/python/core/frugal/server/simple_server.py
+++ lib/python/core/frugal/server/simple_server.py
@@ -1,8 +1,7 @@
 import logging
 
-from thrift.transport import TTransport
-
-from .server import FServer
+from frugal.registry import FServerRegistry
+from frugal.server import FServer
 
 logger = logging.getLogger(__name__)
 
@@ -24,41 +23,34 @@ class FSimpleServer(FServer):
             f_protocol_factory: FProtocolFactory
         """
 
-        self._processor_factory = processor_factory
-        self._transport = transport
-        self._transport_factory = transport_factory
-        self._protocol_factory = protocol_factory
+        self._processor_factory = f_processor_factory
+        self._transport = thrift_server_transport
+        self._transport_factory = f_transport_factory
+        self._protocol_factory = f_protocol_factory
         self._stopped = False
+
+    def serve(self):
+        self._accept_loop()
+
+    def stop(self):
+        self._stopped = True
+        self._transport.close()
 
     def _accept_loop(self):
         while not self._stopped:
-            try:
-                client = self._transport.accept()
-
-
-    def serve(self):
-        while not self._stopped:
+            self._transport.listen()
             client = self._transport.accept()
             if not client:
                 continue
-            itrans = self.inputTransportFactory.getTransport(client)
-            otrans = self.outputTransportFactory.getTransport(client)
+            self._accept(client)
 
-            iprot = self.inputProtocolFactory.getProtocol(itrans)
-            oprot = self.outputProtocolFactory.getProtocol(otrans)
-
-            try:
-                while True:
-                    self.processor.process(iprot, oprot)
-            except TTransport.TTransportException:
-                pass
-            except Exception as x:
-                logger.exception(x)
-
-            itrans.close()
-            otrans.close()
-
-    def _process_requests(self, client_transport):
+    def _accept(self, client_transport):
         processor = self._processor_factory.get_processor(client_transport)
         transport = self._transport_factory.get_transport(client_transport)
         protocol = self._protocol_factory.get_protocol(transport)
+        transport.set_registry(FServerRegistry(processor,
+                                               self._protocol_factory,
+                                               protocol))
+        transport.open()
+
+
