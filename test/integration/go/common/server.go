b9a2d17f13afad5456f251ed65e9ef05555e7a6f
Brian Shannan <brian.shannan@workiva.com>
2016-09-01 16:47:36 -0500

    go: remove deprecated code

--- test/integration/go/common/server.go
+++ test/integration/go/common/server.go
@@ -89,18 +89,6 @@ func StartServer(
 				frugal.NewFProtocolFactory(protocolFactory),
 				frugal.NewFProtocolFactory(protocolFactory)))
 		server = &httpServer{hostPort: hostPort}
-	case "stateful", "stateless-stateful": // @Deprecated TODO: Remove in 2.0
-		fTransportFactory := frugal.NewFMuxTransportFactory(2)
-		server = frugal.NewFNatsServer(
-			conn,
-			fmt.Sprintf("%d", port),
-			time.Minute,
-			processor,
-			fTransportFactory,
-			frugal.NewFProtocolFactory(protocolFactory),
-		)
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
-		go http.ListenAndServe(hostPort, nil)
 	}
 	fmt.Println("Starting %s server...", transport)
 	server.Serve()
