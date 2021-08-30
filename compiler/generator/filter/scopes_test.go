52188febd11f76fbfa6c6c98dc1c0729235bf917
Josh Przybyszewski <josh.przybyszewski@workiva.com>
2021-08-30 10:19:59 -0500

    write unit tests for scopes.go

--- compiler/generator/filter/scopes_test.go
+++ compiler/generator/filter/scopes_test.go
@@ -1,1 +1,24 @@
 package filter
+
+import (
+	"testing"
+
+	"github.com/Workiva/frugal/compiler/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScopesSpecIsSpecified(t *testing.T) {
+	s := &parser.Scope{Name: `updatesScope`}
+	var ss *scopesSpec
+	assert.False(t, ss.isSpecified(s))
+
+	ss = &scopesSpec{}
+	assert.False(t, ss.isSpecified(s))
+
+	b := false
+	ss.All = &b
+	assert.False(t, ss.isSpecified(s))
+
+	b = true
+	assert.True(t, ss.isSpecified(s))
+}
